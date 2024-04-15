cafb8642b6ba4556a04801ce41aa542a38d90cb4
kythpepito <kythroannpepito24@gmail.com>
2024-04-15 19:20:26 +0800

    six commit

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+import { AuthenticationService } from '../authentication.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  userName: string = "";
+  password: string = "";
+  constructor(private alertController: AlertController,private toastController: ToastController, private router: Router, private authenticationService: AuthenticationService) { }
+
+  ngOnInit() {
+  }
+
+  ionViewWillEnter() {
+    this.userName = '';
+    this.password = '';
+  }
+
+  async logIn(){
+
+    const credentials = [
+      { username: 'admin', password: '123' },
+      { username: 'person', password: '456' },
+      { username: 'joe', password: '890' }
+    ];
+
+    const UserExist = credentials.find(user => user.username === this.userName && user.password === this.password);
+    if (UserExist) {
+      localStorage.setItem('username',this.userName);
+      localStorage.setItem('password',this.password);
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Status',
+      message: 'LOG IN SUCCESSFULLY',
+      buttons: ['OK']
+    });
+    await alert.present();
+    this.authenticationService.canProceed = true;
+    this.router.navigate(['tabs/dashboard']);
+  }else{
+    const toast = await this.toastController.create({
+      message: 'Login Failed',
+    });
+    toast.present();
+  }
+
+}
+}
